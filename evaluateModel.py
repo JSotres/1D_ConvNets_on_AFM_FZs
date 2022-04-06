fd912225962fb777fabb783bdcf1f4d19df8e954
Javier <jsotres@gmail.com>
2022-04-07 01:18:12 +0200

    script for evaluating trained models

--- evaluateModel.py
+++ evaluateModel.py
@@ -1,108 +1,82 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from scipy.stats import loguniform
-import math
 import os
 import glob
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Conv1D
-from tensorflow.keras.layers import MaxPooling1D
-
 import pickle
-
 from ResNet50_1D import *
-from ConvNet1D import *
+from ConvNet_1D import *
 
 
 
 if __name__ == "__main__":
 
-    pickle_folder = 'Pickle_Mica_nPoints_5120'
-    pickle_folder_full = f'FZs/Mica/{pickle_folder}'
+    pickle_folder = 'pickle_files_Mica_nPoints_5120'    
+    pickle_folder_full = f'FZs/{pickle_folder}'
 
-    Xi = pickle.load(open(os.path.join(pickle_folder_full,'Xi'), 'rb'))
-    zi = pickle.load(open(os.path.join(pickle_folder_full,'zi'), 'rb'))
-    yi = pickle.load(open(os.path.join(pickle_folder_full,'yi'), 'rb'))
-    X = pickle.load(open(os.path.join(pickle_folder_full,'X'), 'rb'))
-    z = pickle.load(open(os.path.join(pickle_folder_full,'z'), 'rb'))
-    y = pickle.load(open(os.path.join(pickle_folder_full,'y'), 'rb'))
+    sample = 'Mica'
+    model_name ='ConvNet-1D_v1_trained_on_Mica'
+    model_name_short = 'ConvNet-1D'
+
+    evaluation_folder = f'Models/{sample}/{model_name}/evaluation_on_{pickle_folder}'
 
     n_points = 5120
 
-    model_name_1 ='ConvNet1D_v2_trained_on_Mica'
+    model = get_ConvNet1D(n_points)
+    # If you want to train ResNet50-1D, just use:
+    #model = get_ResNet50_1D()
 
+    model.load_weights(f'Models/{sample}/{model_name}/weights_{model_name}_best_val_loss.h5')
     
-    evaluation_folder_1 = f'Models/Mica/{model_name_1}/evaluation_on_{pickle_folder}'
 
-    if os.path.exists(evaluation_folder_1):
-        files1 = glob.glob(f'{evaluation_folder_1}/*')
-        for f in files1:
+    Xi = pickle.load(open(os.path.join(pickle_folder_full,'Xi_val'), 'rb'))
+    zi = pickle.load(open(os.path.join(pickle_folder_full,'zi_val'), 'rb'))
+    yi = pickle.load(open(os.path.join(pickle_folder_full,'yi_val'), 'rb'))
+    X = pickle.load(open(os.path.join(pickle_folder_full,'X_val'), 'rb'))
+    z = pickle.load(open(os.path.join(pickle_folder_full,'z_val'), 'rb'))
+    y = pickle.load(open(os.path.join(pickle_folder_full,'y_val'), 'rb'))
+
+    if os.path.exists(evaluation_folder):
+        files = glob.glob(f'{evaluation_folder}/*')
+        for f in files:
             os.remove(f)
     else:
-        os.mkdir(evaluation_folder_1)
-
-    model1 = get_ConvNet1D(n_points)
-    
-    model1.load_weights(f'Models/Mica/{model_name_1}/weights_{model_name_1}_best_val_loss.h5')
+        os.mkdir(evaluation_folder)
 
     
-
     Xi = np.array(Xi).reshape(-1,n_points,1)
     yi = np.asarray(yi)
-
-    
-
-    y_hat1 = model1.predict(Xi)
-
-    
     yi = np.squeeze(yi)
-    y_hat1 = np.squeeze(y_hat1)
+    
+    y_hat = model.predict(Xi)    
+    y_hat = np.squeeze(y_hat)
 
     
-    mae_list1 = []
-    fz_z_interval = []
-    fz_z_range = []
-
-    
+    error_list = []    
     
     for q in range(yi.shape[0]):
-
-        mae_list1.append ( (z[q][y[q]] - z[q][int(y_hat1[q]*(len(z[q])-1))] ))
-        fz_z_interval.append ( np.abs (z[q][0]-z[q][1]))
-        fz_z_range.append ( np.abs (z[q][0]-z[q][-1]))
-    
-
+        error_list.append ( (z[q][y[q]] - z[q][int(y_hat[q]*(len(z[q])-1))] ))
         
-    mean1 = sum(mae_list1) / len(mae_list1)
-    variance1 = sum([((x - mean1) ** 2) for x in mae_list1]) / len(mae_list1)
-    res1 = variance1 ** 0.5
+    mean = sum(error_list) / len(error_list)
+    variance = sum([((x - mean) ** 2) for x in error_list]) / len(error_list)
+    res = variance ** 0.5
 
-    print("mean1: ", mean1)
-    print("res1: ", res1)
+    print("mean: ", mean)
+    print("res: ", res)
 
     nbins=40
     histRange = (-6,6)
     
-    plt.hist(mae_list1, bins=nbins, range=histRange)
+    plt.hist(error_list, bins=nbins, range=histRange)
     plt.ylabel('counts', fontsize=14)
     plt.xlabel('zc - zc_pred (nm)', fontsize=14)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
-    #plt.title(f'evaluation_on_{pickle_folder}')
-    plt.title(f'ConvNet-1D: evaluation on FZs on Mica test set', fontsize=14)
+    plt.title(f'{model_name_short}: evaluation on FZs on {sample} val set', fontsize=14)
     plt.savefig(os.path.join(
-        f'{evaluation_folder_1}',
-        f'MAE_Raw_{model_name_1}_on_{pickle_folder}.png'))
-    plt.show()
-
-    
-    
+        f'{evaluation_folder}',
+        f'Error_{model_name}_on_{pickle_folder}.png'))
+    plt.show()    
 
     for q in range(len(y)):
         plt.scatter(
@@ -113,20 +87,13 @@ if __name__ == "__main__":
             zorder=2,
             label='zc'
         )
-        plt.scatter(z[q][int(y_hat1[q]*(len(z[q])-1))] , X[q][int(y_hat1[q]*(len(z[q])-1))],
-            s=100,
-            color='k',
-            marker='v',
-            zorder=3,
-            label='zc_pred ConvNet-1D'
-        )
         plt.scatter(
-            z[q][int(y_hat2[q]*(len(z[q])-1))] , X[q][int(y_hat2[q]*(len(z[q])-1))],
+            z[q][int(y_hat[q]*(len(z[q])-1))] , X[q][int(y_hat[q]*(len(z[q])-1))],
             s=100,
             color='r',
             marker='x',
-            zorder=4,
-            label='zc_pred ResNet50-1D'
+            zorder=3,
+            label=f'zc_pred {model_name_short}'
         )
         plt.plot(z[q], X[q], zorder=1, color='y')
         
